c3aa6620125fde7bfebd3bb53b58a64a095e8a31
15 <fifteenai15@gmail.com>
2024-11-30 04:27:03 -0500

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,32 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://2024.marefair.org",
+  prefetch: {
+    prefetchAll: true,
+  },
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    icon(),
+  ],
+  output: "hybrid",
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
+  redirects: {
+    '/schedule': '/schedule/friday',
+  }
+});
